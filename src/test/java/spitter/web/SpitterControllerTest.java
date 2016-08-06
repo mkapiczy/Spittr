2afd74b32632a9b2dfdbed8bb2501d8916dcfee5
mkapiczy <mix.kap@gmail.com>
2016-08-06 17:19:36 +0200

    Register form - bez obsługi POST'a

---
+++ src/test/java/spitter/web/SpitterControllerTest.java
@@ -0,0 +1,19 @@
+package spitter.web;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+public class SpitterControllerTest {
+	
+	@Test
+	public void shouldShowRegistration() throws Exception{
+		SpitterController controller = new SpitterController();
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		
+		mockMvc.perform(get("/spitter/register")).andExpect(view().name("registerForm"));
+	}
+}
